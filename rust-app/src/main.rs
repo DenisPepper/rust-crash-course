2298c69c91a6a7ea3fbfd143702c915f80c993a3
DenisPepper <thech@yandex.ru>
2024-04-03 23:07:31 +0300

    Добавит примеры строковых и числовых переменных

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,5 +1,19 @@
 #![deny(clippy::all)]
 
 fn main() {
-    println!("Hello, world!");
+    // переменные нужно задавать в snake_case
+    // почти всегда rust определит тип данных переменной на основе её значения
+    // let - не позволит переопределить значение идентификатора, кроме случаев затенения
+    // let mut - значение - можно переопределить, тип - нельзя
+    let name = "Denis";
+    println!("My name is {}", name);
+
+    let mut _age: u8 = 30;
+    //let mut _age = 30u8; - тип можно задать после значения
+    let _population = 61_000_000;
+    let _rgb = 0xFF0000;
+
+    let size1 = 5.2f32;
+    let size2 = 9.1f32;
+    let _total_size = size1 + size2;
 }
